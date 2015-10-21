c514fc9000c79de47c9e9188d3ab97d0482deee4
Bence Dányi <bence@danyi.me>
2015-10-21 17:18:00 +0200

    redux

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import { createStore, applyMiddleware } from 'redux';
+import { connect, Provider } from 'react-redux';
+
 class MessageList extends React.Component {
   render() {
     return (
@@ -13,8 +16,12 @@ class MessageList extends React.Component {
 
 class Message extends React.Component {
   render() {
+    if (this.props.pending) {
+      return <li>...</li>;
+    }
+
     return (
-      <li><span>{this.props.from}</span>{this.props.text}</li>
+      <li><span>{this.props.from}</span>: {this.props.text}</li>
     );
   }
 }
@@ -23,8 +30,8 @@ class MessageForm extends React.Component {
   render() {
     return (
       <div>
-        <input placeholder='message' />
-        <button>send</button>
+        <input placeholder='message' ref='message'/>
+        <button onClick={() => this.props.onSend('foo', this.refs.message.value)}>send</button>
       </div>
     );
   }
@@ -35,15 +42,57 @@ class ChatWindow extends React.Component {
     return (
       <div>
         <MessageList messages={this.props.messages} />
-        <MessageForm />
+        <MessageForm onSend={(from, text) => this.props.dispatch({
+          type: ADD_MESSAGE,
+          payload: {
+            from,
+            text,
+          },
+        })}/>
       </div>
     );
   }
 }
 
-const messages = [{
-  from: 'foo',
-  text: 'bar',
-}]
+const server = store => next => action => {
+  switch (action.type) {
+    case ADD_MESSAGE:
+      setTimeout(() => {
+        store.dispatch({
+          type: ACK_MESSAGES,
+        });
+      }, 1000);
+  }
+  return next(action);
+}
 
-render(<ChatWindow messages={messages} />, document.getElementById('mount'));
+const store = applyMiddleware(server)(createStore)((state, action) => {
+  switch (action.type) {
+    case ADD_MESSAGE: return {
+      messages: state.messages.concat({
+        pending: true,
+        from: action.payload.from,
+        text: action.payload.text,
+      }),
+    };
+    case ACK_MESSAGES: return {
+      messages: state.messages.map(message => ({ ...message, pending: false })),
+    };
+  }
+  return state;
+}, {
+  messages: [{
+    from: 'foo',
+    text: 'bar',
+  }]
+});
+
+const WrappedChat = connect(x => x)(ChatWindow);
+
+render(
+  <Provider store={store}>
+    <WrappedChat />
+  </Provider>, document.getElementById('mount'));
+
+const ADD_MESSAGE = 'ADD_MESSAGE';
+const ACK_MESSAGES = 'ACK_MESSAGES';
